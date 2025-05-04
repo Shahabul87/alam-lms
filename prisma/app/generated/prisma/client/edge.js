ace2fc551d6f9556209755131032ed0ae443c291
Shahabul <sham251087@gmail.com>
2025-05-04 13:43:05 -0700

    after updating database connection

--- prisma/app/generated/prisma/client/edge.js
+++ prisma/app/generated/prisma/client/edge.js
@@ -1129,7 +1129,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../../../.env"
   },
   "relativePath": "../../../..",
--- prisma/app/generated/prisma/client/index.js
+++ prisma/app/generated/prisma/client/index.js
@@ -1130,7 +1130,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../../../.env"
   },
   "relativePath": "../../../..",
